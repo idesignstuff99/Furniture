ae3204d1897351ea047006647fff67cdb0b18969
idesignstuff99 <75559510+idesignstuff99@users.noreply.github.com>
2022-02-20 21:57:06 -0800

    Add files via upload

---
+++ email.js
@@ -0,0 +1,10 @@
+
+    $.ajax({
+      url: "https://formsubmit.co/ajax/idesignstuff99@gmail.com",
+      method: "POST",
+      data: {
+          name: "FormSubmit",
+          message: "Correo electronico"
+      },
+      dataType: "json"
+  });
